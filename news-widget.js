136487fc8203ca07c6cb837bab61e6b5f405106d
EmillioHezekiah <122629935+EmillioHezekiah@users.noreply.github.com>
2024-08-20 10:29:45 +0700

    Update news-widget.js

--- news-widget.js
+++ news-widget.js
@@ -63,10 +63,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Add the image and title before news articles
             widget.innerHTML = `
-                <img src="https://github.com/EmillioHezekiah/news-widget-2/blob/18d2e9e6bacf0775095d6e1f8c5a81d051cb4bac/trade2372.png?raw=true" 
-                     alt="PR News Logo" 
-                     style="display: block; margin: 0 auto; width: 128px; height: 128px;">
-                <h2>News from Trade PR</h2>
+                <div style="display: flex; align-items: center;">
+                    <img src="https://github.com/EmillioHezekiah/news-widget-2/blob/18d2e9e6bacf0775095d6e1f8c5a81d051cb4bac/trade2372.png?raw=true" 
+                         alt="PR News Logo" 
+                         style="width: 128px; height: 128px; margin-right: 15px;">
+                    <h2>News from Trade PR</h2>
+                </div>
             `;
 
             if (articles.length === 0) {
